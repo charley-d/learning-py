793590cf1cc5d00d5c8c62b1994cacff82654be6
charley-d <67061817+charley-d@users.noreply.github.com>
2021-05-20 15:27:45 +0100

    Create asterisk_triangle.py

---
+++ asterisk_triangle.py
@@ -0,0 +1,14 @@
+"""
+Created on Tue Oct 31 15:25:40 2017
+
+@author: cdib
+"""
+
+#Asterisk triangle
+
+n = int(input("Enter a number: "))
+x = 1
+while x <= n:
+    print("*" * x)
+    x = x + 1
+    
